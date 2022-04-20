91ac70bfed7ab9540a755a9c17517d2946f35c6d
fermer123 <fermer_525@mail.ru>
2022-04-20 19:04:55 +0500

    dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  context: 'src',
   mode: 'development',
   entry: './src/index.js',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
