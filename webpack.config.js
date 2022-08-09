b36ff5e08dadfc66f342ebf06754443278946c5e
fermer123 <fermer_525@mail.ru>
2022-08-09 16:26:46 +0500

    new

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: './main.js',
+  mode: 'production',
+  entry: {
+    //какие файлы выбираю из папки src и помещаю в папку dist
+    filename: path.resolve(__dirname, 'src/index.js'), //полный путь до папки методом path + обьединение методом resolve
+  },
   output: {
-    filename: '[name].js',
+    //куда перекидываю файил и какое у него название
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[hash][ext][query]',
+    filename: '[name][contenthash].js',
+    assetModuleFilename: 'img/[name][ext]', //куда и под каким названием складываю
+    clean: true,
+  },
+  performance: {
+    //убрать уведомления
+    hints: false, //указываем подсказки отключены
+    maxAssetSize: 512000, //указываем макс рамер изобр
+    maxEntrypointSize: 512000, //если превышает то подгрузка после загрузки сайта
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
+    new htmlWebpackPlugin({
+      title: 'Webpack',
+      filename: 'index.html',
+      template: 'src/index.html',
     }),
     new CleanWebpackPlugin(),
   ],
@@ -29,7 +41,7 @@ module.exports = {
       },
       {
         test: /\.(svg|jpg|png)$/i,
-        type: 'asset/resource',
+        type: 'asset/resource', //если подобный файл то изображение
       },
       {
         test: /\.(ttf|otf)$/i,
